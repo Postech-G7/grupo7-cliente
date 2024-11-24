e43d3b1d380c7badccd9c1e1b56f7807789e8003
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2024-11-24 18:44:22 -0300

    refactor: swagger.ts

--- src/swagger.ts
+++ src/swagger.ts
@@ -4,6 +4,4 @@ import { swagger } from "swagger/swagger";
 const outputFile = "./swagger/swagger.json";
 const routes = ["./configuration/routes.config.ts"];
 
-swaggerAutogen()(outputFile, routes, swagger).then(() => {
-  console.log("Swagger gerado com sucesso!");
-});
+swaggerAutogen()(outputFile, routes, swagger);
