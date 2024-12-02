e9fffcc315ca0dc20a370327a44a5d87ae9ea02a
roberto furlan <furlanrgf@gmail.com>
2024-12-02 18:01:57 -0300

    feat: add MySQL support and update environment configuration

--- src/domains/cliente/adapter/driver/rest/swagger/acesso.swagger.ts
+++ src/domains/cliente/adapter/driver/rest/swagger/acesso.swagger.ts
@@ -7,3 +7,8 @@ export const put_cliente = {
     $email: "reis@gmail.com",
     $nome: "Luan Reis"
 }
+
+export const post_cliente_autenticacao = {
+    $cpf: "12345678909",
+    $email: "reis@gmail.com",
+}
